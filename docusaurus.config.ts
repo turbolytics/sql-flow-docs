5d05e1f49175f9bb78629b475e8a090eccc2e79c
turbolytics.io <danny@turbolytics.io>
2025-02-07 06:32:49 -0500

    image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/logo.svg',
     navbar: {
       title: 'SQLFlow Documentation',
       logo: {
